583dda1fcd87c0b5dbf3fdcd3707e5a0bf7311af
Brian Bengtson <brian@bbengt.com>
2014-11-25 22:17:06 -0600

    fixed albumSearch() and code cleanup
    -Fixed albumSearch() so that it now works properly
    -Cleaned up code to make it consistent
    -Modified output
    -Changed behavior of interface so the user can search multiple times
    in a row

--- MusicSearch.java
+++ MusicSearch.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 /*
  * What's left to do: 
- * 	-Implement album search
  * 	-Javadoc
  * 	-Code clean-up
  * 	-Testing/bugfixes
@@ -21,8 +20,10 @@ public class MusicSearch {
 		
 		while(true) {
 			
-			System.out.print("What are you looking for (artist, album, song)? ");
+			System.out.print("What are you looking for (artist, album, song)? If you're done, enter 'quit': ");
 			String searchBy = in.nextLine();
+			if(searchBy.equalsIgnoreCase("quit"))
+				break;
 			System.out.print("Enter search term: ");
 			String term = in.nextLine();
 			System.out.print("What's the path to your music library? ");
@@ -49,7 +50,7 @@ public class MusicSearch {
 					System.out.println("Artist not found.");
 					
 				}
-				break;
+				continue;
 				
 			} else if(searchBy.equalsIgnoreCase("album")) {
 				
@@ -58,7 +59,7 @@ public class MusicSearch {
 					System.out.println("Album not found.");
 				}
 			
-				break;
+				continue;
 			} else if(searchBy.equalsIgnoreCase("song")) {
 				
 				boolean success = songSearch(term, path);
@@ -66,8 +67,7 @@ public class MusicSearch {
 					System.out.println("Song not found.");
 				}
 				
-				break;
-				
+				continue;
 			} else {
 				System.out.println("Invalid search type.");
 				System.out.println();
@@ -78,110 +78,121 @@ public class MusicSearch {
 
 	}
 	
-	private static boolean artistSearch(String artist, String searchPath) {
-		
-		if(artist == null) {
-			System.out.println("Invalid artist.");
-			return false;
-		}
-		if(!new File(searchPath).exists()) {
-			System.out.println("Invalid search path.");
-			return false;
-		}
-
+	private static boolean artistSearch(String artist, String searchPath) 
+	{
 		ArrayList<String> list = new ArrayList<String>();
-
-		for(File file : new File(searchPath).listFiles()) {
-
+		
+		for(File file : new File(searchPath).listFiles()) 
+		{
+		
 			// Artist should be a directory
-			if(file.isDirectory()) {
-				
-				// Filename contains (ignoring case) artist name
-				if(file.getName().toLowerCase().contains(artist.toLowerCase())) {
+			if(file.isDirectory()) 
+			{
+				// Filename contains (ignoring case) artist name or if Filename has underscores instead of spaces
+				if(file.getName().toLowerCase().contains(artist.toLowerCase())||file.getName().toLowerCase().contains(artist.toLowerCase().replaceAll(" ", "_"))) 
+				{
 					list.add(file.getPath());
-				}
-				
-				// Filename has underscores instead of spaces
-				if(file.getName().toLowerCase().contains(artist.toLowerCase().replaceAll(" ", "_"))) {
-					list.add(file.getPath());
+					System.out.println();
+					System.out.println("The artist, " + file.getName() + ", was found.");
+					System.out.println("The content found for " + file.getName() + " includes: ");
 				}
 			}
-		}
-
-		// If we've gotten through all files/folders in the specified path and haven't found the artist, it doesn't exist
-		if(list.isEmpty()) {
-			return false;
 		}
 		
+		// If we've gotten through all files/folders in the specified path and haven't found the artist, it doesn't exist
+		if(list.isEmpty()) 
+			return false;
 		// Search through each artist that matched the search pattern
-		for(String path : list) {
-			
+		for(String path : list) 
+		{
 			// Search through each album by the artist
-			for(File album : new File(path).listFiles()) {
-				
+			for(File album : new File(path).listFiles()) 
+			{
 				// Skip any non-directories (e.g. file in artist folder but not in an album folder)
-				if(!album.isDirectory()) {
+				if(!album.isDirectory()) 
 					continue;
-				}
-				
 				System.out.println();
-				System.out.println(album.getPath());
+				System.out.println("Album: " + album.getName());
 				System.out.println();
-				
+				System.out.println("The path to this album is: " + album.getPath());
+				System.out.println();
+				System.out.println("The content found in " + album.getName() + " includes: ");
 				for(File song : album.listFiles()) {
-					
-					// Skip directories
-					if(!song.isFile()) {
-						continue;
-					}
-					System.out.println(song.getName());
+				// Skip directories
+				if(!song.isFile())
+					continue;
+				System.out.println(song.getName());
 				}
 			}
 		}
-
+		System.out.println("_________________________________________________________________________________________________________________________________________________________________________________________________________________");
+		
 		return true;
 	}
 	
-	private static boolean albumSearch(String album, String path) {
-		return false;
-	}
-	
-	private static boolean songSearch(String song, String path) {
-		
-		if(song == null) {
-			System.out.println("Invalid song.");
-			return false;
-		}
-		if(!new File(path).exists()) {
-			System.out.println("Invalid path.");
-			return false;
-		}
-		
+	private static boolean albumSearch(String album, String searchPath) 
+	{
 		boolean success = false;
-		
-		File dir = new File(path);
-		for(File file : dir.listFiles()) {
-
-			if(file.isDirectory()) {
-				if(success) {
-					songSearch(song, file.getPath());
-				} else {
-					success = songSearch(song, file.getPath());
+	
+		for(File artists : new File(searchPath).listFiles()) 
+		{	
+			if(!artists.isDirectory())
+				continue;
+			for(File albums : artists.listFiles())
+			{
+				if(albums.isDirectory())
+				{
+					if(albums.getName().toLowerCase().contains(album.toLowerCase())||albums.getName().toLowerCase().contains(album.toLowerCase().replaceAll(" ", "_")))
+					{
+						System.out.println();
+						System.out.println("The album, " + albums.getName() + ", was found.");
+						System.out.println();
+						System.out.println("The path to this album is: " + albums.getPath());
+						System.out.println();
+						System.out.println("The content found in " + albums.getName() + " includes: ");
+						for(File song : albums.listFiles()) 
+						{
+							if(!song.isFile())
+							continue;
+							System.out.println(song.getName());
+							success = true;
+						}
+					}	
 				}
 			}
-			if(file.isFile()) {
-				if(file.getName().toLowerCase().contains(song.toLowerCase())) {
-					System.out.println(file.getPath());
-					success = true;	
-					continue;
-				}
-				if(file.getName().toLowerCase().contains(song.toLowerCase().replaceAll(" ", "_"))) {
-					System.out.println(file.getPath());
-					success =  true;
+		}
+		System.out.println("_________________________________________________________________________________________________________________________________________________________________________________________________________________");
+	
+		return success;
+	}
+
+
+	
+	private static boolean songSearch(String song, String path) 
+	{
+		boolean success = false;
+		File dir = new File(path);
+		for(File file : dir.listFiles()) 
+		{
+			if(file.isDirectory()) 
+			{
+				if(success) 
+					songSearch(song, file.getPath());
+				else 
+					success = songSearch(song, file.getPath());
+			}
+			if(file.isFile()) 
+			{
+				if(file.getName().toLowerCase().contains(song.toLowerCase())||file.getName().toLowerCase().contains(song.toLowerCase().replaceAll(" ", "_"))) 
+				{
+				System.out.println("The song, " + file.getName() + ", was found.");
+				System.out.println();
+				System.out.println("The path to this song is " + file.getPath());
+				System.out.println("_________________________________________________________________________________________________________________________________________________________________________________________________________________");
+				success = true;	
 				}
 			}
 		}
 		return success;
+		}
 	}
-	
-}
